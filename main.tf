d3a30c2587a951f968e44e90033090ceb9741516
Ilsun Choi <isc0304@naver.com>
2024-07-19 09:05:40 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,13 +23,13 @@ variable "resource_group" {
 
 variable "location" {
   type = string
-  default = "eastus"
+  default = "australiaeast"
 }
  
 
 resource "azurerm_cosmosdb_account" "db" {
   name                = "ine-cosmos-db-data-${random_id.randomId.dec}"
-  location            = "eastus"
+  location            = "australiaeast"
   resource_group_name = var.resource_group
   offer_type          = "Standard"
   kind                = "GlobalDocumentDB"
@@ -45,7 +45,7 @@ resource "azurerm_cosmosdb_account" "db" {
   }
 
   geo_location {
-    location          = "eastus"
+    location          = "australiaeast"
     failover_priority = 0
   }
 }
